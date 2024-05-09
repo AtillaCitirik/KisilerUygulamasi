be386752c100a020ca623fa7d4e318e605a224ad
atilla.citirik <atillactrk@gmail.com>
2024-05-09 15:47:36 +0300

    Initial Commit

---
+++ KisilerUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KisilerUygulamasi
+//
+//  Created by Atilla Çıtırık on 9.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
