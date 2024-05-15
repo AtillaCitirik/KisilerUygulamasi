052e9299df48f03d610288a139d8ab48d6718df0
atilla.citirik <atillactrk@gmail.com>
2024-05-16 00:43:28 +0300

    Detay sayfasına veri transferi

--- KisilerUygulamasi/ui/views/Anasayfa.swift
+++ KisilerUygulamasi/ui/views/Anasayfa.swift
@@ -14,6 +14,19 @@ class Anasayfa: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonDetay(_ sender: Any) {
+        let kisi = Kisiler(kisi_id: 1, kisi_ad: "ahmet", kisi_tel: "1111")
+        performSegue(withIdentifier: "toDetay", sender: kisi)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetay" {
+            if let kisi = sender as? Kisiler {
+                let gidilecekVC = segue.destination as! KisiDetay
+                gidilecekVC.kisi = kisi
+            }
+        }
+    }
+    
 }
 
