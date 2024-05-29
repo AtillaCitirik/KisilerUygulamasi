3023b0ade061f59784b6126f8e79b1be0bc644b6
atilla.citirik <atillactrk@gmail.com>
2024-05-29 16:01:44 +0300

    DB update

--- KisilerUygulamasi/data/Repo/KisilerdaoRepository.swift
+++ KisilerUygulamasi/data/Repo/KisilerdaoRepository.swift
@@ -35,7 +35,16 @@ class KisilerdaoRepository {
     }
     
     func guncelle(kisi_id:Int, kisi_ad:String, kisi_tel:String) {
-        print("Kişi Güncelle : \(kisi_id) - \(kisi_ad) - \(kisi_tel)")
+        db?.open()
+
+        do {
+            try db!.executeUpdate(" UPDATE kisiler SET kisi_ad = ?, kisi_tel = ? WHERE kisi_id = ?", values: [kisi_ad,kisi_tel, kisi_id])
+        }catch{
+            print(error.localizedDescription)
+        }
+  
+        db?.close()
+        
     }
     
     func sil(kisi_id:Int){
